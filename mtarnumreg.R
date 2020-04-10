3ba3a7a20784f8b9b3d85374d2af519d7a054554
Andrey Rincon <adrincont@unal.edu.co>
2020-04-10 17:06:24 -0500

    Update mtarnumreg.R

--- mtarnumreg.R
+++ mtarnumreg.R
@@ -66,20 +66,21 @@ if (nu == 0) {
 # Necesaria ----#
 dwishartB = function(x, nu, S){
   k = ncol(x)
-  producto = as.brob(1)
+  producto = Brobdingnag::as.brob(1)
   for (i in 1:k) {
-    producto = producto*exp(as.brob(lgamma((nu + 1 - i)/2)))
+    producto = producto*exp(Brobdingnag::as.brob(lgamma((nu + 1 - i)/2)))
   }
-  densidades = (as.brob(2)^(nu*k/2)*as.brob(pi^(k*(k - 1)/4))*producto)^(-1)*
-    as.brob(det((1/nu)*S))^(-nu/2)*as.brob(det(x))^((nu - k - 1)/2)*
-    exp(as.brob(-0.5*sum(diag(solve((1/nu)*S) %*% x))))
+  densidades = (Brobdingnag::as.brob(2)^(nu*k/2)*Brobdingnag::as.brob(pi^(k*(k - 1)/4))*producto)^(-1)*
+    Brobdingnag::as.brob(det((1/nu)*S))^(-nu/2)*Brobdingnag::as.brob(det(x))^((nu - k - 1)/2)*
+    exp(Brobdingnag::as.brob(-0.5*sum(diag(solve((1/nu)*S) %*% x))))
   return(densidades)
 }
 dmnormB = function(x, mean, sigma){
+  sigma = as.matrix(sigma)
   dist = Brobdingnag::as.brob(t(x - mean) %*% solve(sigma) %*% (x - mean))
-  # cte = (2*pi)^{-nrow(sigma)/2}*determinant(sigma, logarithm = FALSE)$modulus^{-1/2}
-  # return(cte*exp(-1/2*dist))
-  return(exp(-1/2*dist))
+  cte = (2*pi)^{-nrow(sigma)/2}*determinant(sigma, logarithm = FALSE)$modulus^{-1/2}
+  return(cte*exp(-1/2*dist))
+  #return(exp(-1/2*dist))
 }
 # Simulacion de previas -------------------------------------------#
 lists = function(l, r, pjmax, qjmax, djmax, ...){
