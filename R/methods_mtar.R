f81e16039ff54e1ef4bd0a241ca035c1f582f742
adrincont <adrincont@unal.edu.co>
2020-05-17 16:48:34 -0500

    ttt

--- R/methods_mtar.R
+++ R/methods_mtar.R
@@ -1,6 +1,6 @@
-autoplot = function(object, ...){ UseMethod("autoplot")}
+autoplot = function(object, ...) UseMethod("autoplot")
 
-autoplot.regim_model = function(object, type = 1) {
+autoplot.regim_model = function(object, type = 1,...) {
   if (!requireNamespace('ggplot2', quietly = TRUE)) {
     stop('ggplot2 is needed for this function to work')
   }else {
@@ -125,7 +125,7 @@ autoplot.regim_model = function(object, type = 1) {
     return(p)
   }
 }
-autoplot.regim_missing = function(object, type = 1) {
+autoplot.regim_missing = function(object, type = 1,...) {
     if (!requireNamespace('ggplot2', quietly = TRUE)) {
       stop('ggplot2 is needed for this function to work')
     }else {
@@ -167,7 +167,7 @@ autoplot.regim_missing = function(object, type = 1) {
       return(p)
     }
   }
-autoplot.tsregim = function(object, type = 1) {
+autoplot.tsregim = function(object, type = 1,...) {
   if (!requireNamespace('ggplot2', quietly = TRUE)) {
     stop('ggplot2 is needed for this function to work')
   }else {
@@ -233,7 +233,7 @@ base::registerS3method('autoplot','regim_model', autoplot.regim_model)
 base::registerS3method('autoplot','regim_missing', autoplot.regim_missing)
 base::registerS3method('autoplot', 'tsregim', autoplot.tsregim)
 
-print = function(object) UseMethod('print')
+print = function(object, ...) UseMethod('print')
 
 print.tsregim = function(object,...){
   cat('Threshold time series:\n','N =',object$N,'\n')
